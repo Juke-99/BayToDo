84275a3c4b5ae1e84169175d0389267a9fca5dc8
如月二十日 <kisaragihatuka@kisaraginoMacBook-Pro.local>
2017-06-27 23:28:15 +0900

    Initial Commit

---
+++ BayToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BayToDo
+//
+//  Created by 如月 二十日 on 2017/06/27.
+//  Copyright © 2017年 HAL東京. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
